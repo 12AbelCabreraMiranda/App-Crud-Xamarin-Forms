8446e74b29926bcd96a4475fb09818efb3de75ca
12AbelCabreraMiranda <leg.abelr@gmail.com>
2021-02-11 23:12:03 -0600

    Crud XamarinForms y SQLite, sin EntityFramework

---
+++ ToDo.App/ToDo.App/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ToDo.App.Data;
+using ToDo.App.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDo.App
+{
+    public partial class App : Application
+    {
+        public static DataBaseContext Context { get; set; }
+        public App()
+        {
+            InitializeComponent();
+            InitializeDataBase();
+            MainPage = new NavigationPage(new HomePage());
+        }
+
+        //Configuracion de Base de datos locales
+        private void InitializeDataBase()
+        {
+            var folderApp = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var dbPath = System.IO.Path.Combine(folderApp, "ToDo.db3");
+            Context = new DataBaseContext(dbPath);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ToDo.App/ToDo.App/Data/DataBaseContext.cs
@@ -0,0 +1,37 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.App.Models;
+
+namespace ToDo.App.Data
+{
+    public class DataBaseContext
+    {
+        public readonly SQLiteAsyncConnection Connection;
+        public DataBaseContext(string path)
+        {
+            Connection = new SQLiteAsyncConnection(path);
+            Connection.CreateTableAsync<ToDoItem>().Wait();
+        }
+
+        //Method Insert
+        public async Task<int> InsertItemAsync(ToDoItem item)
+        {
+            return await Connection.InsertAsync(item);
+        }
+
+        //Method List View
+        public async Task<List<ToDoItem>> GetItemsAsync()
+        {
+            return await Connection.Table<ToDoItem>().ToListAsync();
+        }
+
+        //Method delete
+        public async Task<int> DeleteItemAzync(ToDoItem item)
+        {
+            return await Connection.DeleteAsync(item);
+        }
+    }
+}
